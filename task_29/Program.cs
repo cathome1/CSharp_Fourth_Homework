47cb2e4d2de6287afea23cb44058fe81d816b519
cathome <cathome.bro@yandex.ru>
2022-08-22 12:57:37 +0300

    third task finished

---
+++ task_29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+int[] array = new int[8];
+void FillArray(int[] a)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        a[i] = new Random().Next();
+    }
+}
+void PrintArray(int[] ar)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < 8; i++)
+    {
+       if (i < 7) Console.Write($"{ar[i]}, ");
+       else Console.WriteLine($"{ar[i]}]");
+    }
+}
+FillArray(array);
+PrintArray(array);
